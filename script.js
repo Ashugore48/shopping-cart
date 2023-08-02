5d055958e853c9eb7c108885523a2d3b9f856404
Ashugore48 <goreashutosh4494@gmail.com>
2023-08-02 23:12:26 +0530

    made changes in all links

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.getElementById("my-cart").addEventListener("click", () => {
   alert("To check your cart you have to Login first");
 });
 document.getElementById("login-btn").addEventListener("click", () => {
-  window.location.href = "/login";
+  window.location.href = "/shopping-cart/login/index.html";
 });
 document.getElementById("signup-btn").addEventListener("click", () => {
-  window.location.href = "/signup";
+  window.location.href = "/shopping-cart/signup/signup.html";
 });
