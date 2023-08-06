ff4a4e2aac90b05bbd04460679a1a27e58de4bb2
Ashugore48 <goreashutosh4494@gmail.com>
2023-08-06 15:45:42 +0530

    final profile commit

--- profile/profile-script.js
+++ profile/profile-script.js
@@ -1,7 +1,29 @@
+// Function to update the formData array in local storage
+function updateFormData(newCurrentUser) {
+  const key = "formData";
+  const arrayJSON = localStorage.getItem(key);
+  const myArray = JSON.parse(arrayJSON) || [];
+
+  if (Array.isArray(myArray)) {
+    const objectToDelete = newCurrentUser.email;
+    const indexToDelete = myArray.findIndex((item) => item.email === objectToDelete);
+
+    if (indexToDelete !== -1) {
+      myArray.splice(indexToDelete, 1);
+    }
+
+    myArray.push(newCurrentUser);
+    localStorage.setItem(key, JSON.stringify(myArray));
+  } else {
+    console.error("formData is not an array.");
+  }
+}
+
 const currentUserString = JSON.parse(localStorage.getItem("currentUser"));
 
 if (currentUserString !== null) {
   let formData = JSON.parse(localStorage.getItem("formData")) || [];
+
   document.getElementById("save-info-button").addEventListener("click", (e) => {
     e.preventDefault();
     let newFirstName = document.getElementById("firstname-input").value;
@@ -13,62 +35,39 @@ if (currentUserString !== null) {
       password: currentUserString.password,
     };
     localStorage.setItem("currentUser", JSON.stringify(newCurrentUser));
-
-    
-
-    const key = "formData";
-    const arrayJSON = localStorage.getItem(key);
-
-    const myArray = JSON.parse(arrayJSON);
-    const objectToDelete = currentUserString.email;
-    const indexToDelete = myArray.findIndex(
-      (item) => item.email === objectToDelete
-    );
-
-    if (indexToDelete !== -1) {
-      myArray.splice(indexToDelete, 1);
-      const updatedArrayJSON = JSON.stringify(myArray);
-      localStorage.setItem(key, updatedArrayJSON);
-    }
-
-    formData.push(newCurrentUser);
-    localStorage.setItem("formData", JSON.stringify(formData));
+    updateFormData(newCurrentUser);
 
     alert("Information saved successfully!!");
     document.getElementById("firstname-input").value = "";
     document.getElementById("lastname-input").value = "";
   });
-  let TruePass = currentUserString.password;
-  document
-    .getElementById("change-password-button")
-    .addEventListener("click", (e) => {
-      e.preventDefault();
-      let oldPass = document.getElementById("old-password-input").value;
-      let newPass = document.getElementById("new-password-input").value;
-      let confirmPass = document.getElementById(
-        "confirm-new-password-input"
-      ).value;
-      if (newPass !== confirmPass) {
-        alert("New Password and Current Passwords are not matching");
-      }
-      if (oldPass === TruePass) {
-        let newCurrentUser = {
-          name: currentUserString.name,
-          lastName: currentUserString.lastName,
-          email: currentUserString.email,
-          password: confirmPass,
-        };
-        localStorage.setItem("currentUser", JSON.stringify(newCurrentUser));
-        formData.push(newCurrentUser);
-        localStorage.setItem("formData", JSON.stringify(formData));
-        alert("Password change successfully!!");
-        document.getElementById("old-password-input").value = "";
-        document.getElementById("new-password-input").value = "";
-        document.getElementById("confirm-new-password-input").value = "";
-      } else {
-        alert("old password is not matching");
-      }
-    });
+
+  document.getElementById("change-password-button").addEventListener("click", (e) => {
+    e.preventDefault();
+    let oldPass = document.getElementById("old-password-input").value;
+    let newPass = document.getElementById("new-password-input").value;
+    let confirmPass = document.getElementById("confirm-new-password-input").value;
+
+    if (newPass !== confirmPass) {
+      alert("New Password and Current Passwords are not matching");
+    } else if (oldPass === currentUserString.password) {
+      let newCurrentUser = {
+        name: currentUserString.name,
+        lastName: currentUserString.lastName,
+        email: currentUserString.email,
+        password: confirmPass,
+      };
+      localStorage.setItem("currentUser", JSON.stringify(newCurrentUser));
+      updateFormData(newCurrentUser);
+
+      alert("Password changed successfully!!");
+      document.getElementById("old-password-input").value = "";
+      document.getElementById("new-password-input").value = "";
+      document.getElementById("confirm-new-password-input").value = "";
+    } else {
+      alert("Old password is not matching");
+    }
+  });
 
   document.getElementById("logout-button").addEventListener("click", (e) => {
     localStorage.removeItem("currentUser");
@@ -76,5 +75,5 @@ if (currentUserString !== null) {
   });
 } else {
   window.location.href = "/shopping-cart/";
-  alert("user not found");
+  alert("User not found");
 }
